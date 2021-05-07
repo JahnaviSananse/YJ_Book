5b8e4aea46ed0857c7f76e628e5be5ddb8125240
unknown <jahnavisananse@gmail.com>
2021-05-07 18:42:26 +0530

    unnecessary folder removed

--- index.js
+++ index.js
@@ -5,9 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// import demoApp from './Hooks/useState/demoApp';
 import AppStack from './src/AppStack';
-import demoApp from './Hooks/useReducer/demoState';
-import demoReducer from './Hooks/useReducer/demoReducer';
-
-AppRegistry.registerComponent(appName, () => demoReducer);
+AppRegistry.registerComponent(appName, () => AppStack);
