a1f435b1b5545cce1120e363fa301cbc5c9f3e83
unknown <jahnavisananse@gmail.com>
2021-05-08 23:23:27 +0530

    understanding of custom hook logic

--- src/Hooks/Auth.hooks.js
+++ src/Hooks/Auth.hooks.js
@@ -2,10 +2,15 @@ import {useState} from 'react';
 
 export const useAuthHook = initialData => {
   const [data, setData] = useState(initialData);
-
-  const onChange = (item, value) => {
+  // {email : '' , password:'' , name : ''}
+  //onChange = {(text)=>setData(text)}
+  const onChange = (name, value) => {
     let obj = {};
-    obj[item] = value;
+    obj[name] = value;
+    console.log('obj>>>', obj);
+    // name : 'abc'
+    // email : 'dcdcdcsd'
+    // pass : 'ddcsdcd'
     setData({...data, ...obj});
   };
 
